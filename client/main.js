086cec3e04c2f24ea99b0c00e4ece85121b91637
seohyun0620 <tjgus7924@naver.com>
2023-01-19 15:03:37 +0900

    서버 연결, 서버 통신

--- client/main.js
+++ client/main.js
@@ -4,11 +4,13 @@ import {
   xhrPromise, */
   tiger,
   delayP,
-  getNode,
+  getNode as $,//getnode를 $로 대체해서 쓸 수 있음
   insertLast,
   changeColor,
   renderSpinner,
-  renderUserCard
+  renderUserCard,
+  renderEmptyCard,
+  attr
 } from "./lib/index.js";
 
 //유저 카드 생성
@@ -17,7 +19,7 @@ import {
 //userList.js에서 renderUserCard함수 만들고 만들어진 함수 안에
 //createUserCard를 던지고, renderUserCard함수를 사용했을 때 렌더링이 잘 되게
 
-const userCardContainer=getNode('.user-card-inner');
+const userCardContainer=$('.user-card-inner');
 
 async function rendingUserList(){
   
@@ -25,9 +27,9 @@ async function rendingUserList(){
 
   try{
     await delayP(2100)
-    getNode('.loadingSpinner').remove();
+    $('.loadingSpinner').remove();
 
-  let response=await tiger.get('https://jsonplaceholder.typicode.com/users')
+  let response=await tiger.get(' http://localhost:3000/users')
   
   let userData=response.data;
   //console.log(userData);
@@ -44,9 +46,34 @@ async function rendingUserList(){
     stagger:0.2,
   })
 }catch(err){
-console.log(err);
+//console.log(err);
+renderEmptyCard(userCardContainer);
 }
 }
 
 
 rendingUserList();
+
+//삭제버튼 누르면 콘솔에 '삭제'글자가 출력될 수 있도록 만들어주세영
+
+function handler(e){ //부모한테 이벤트 위임
+  //console.log(e.target.closest('button')); //버튼 말고 다른 곳 누르면 null,버튼 누르면 버튼 출력(가장 가까운 button 누르기?)
+
+  let deleteButton=e.target.closest('button'); //누른 대상에 가까운 button
+  let article=e.target.closest('article'); //대상에 인접한 article
+
+  if(!deleteButton||!article) return;//삭제버튼 누르면 버튼이면서 아티클
+  
+  let id=attr(article,'data-index').slice(5);
+
+  tiger.delete(` http://localhost:3000/users/${id}`).then(()=>{
+    userCardContainer.innerHTML='';
+    rendingUserList();
+  })
+  //console.log(deleteButton);
+}
+
+userCardContainer.addEventListener('click',handler)
+/* userCardContainer.addEventListener('click',(e)=>{
+  console.log(e.target);
+}) */
